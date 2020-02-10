c95285f777f2be0a6ee638a845a93493103ef97b
yuhani20 <nahuyw@gmail.com>
2020-02-10 17:10:19 +0800

    Commit Menu.class, Menu.java only

--- src/main/Menu.java
+++ src/main/Menu.java
@@ -21,15 +21,28 @@ public class Menu extends MouseAdapter {
         int mX=e.getX();
         int mY=e.getY();
 
-        if (mouseOver(mX,mY)){
-            Game.state=STATE.Game;
-            handler.addObject(new Player(r.nextInt(Game.WIDTH2), r.nextInt(Game.HEIGHT2), ID.Player, handler));
-            handler.addObject(new BasicEnemy(r.nextInt(Game.WIDTH2), r.nextInt(Game.HEIGHT2), ID.BasicEnemy, handler));
+        if(Game.state==STATE.Menu) {
+            if (mouseOver(mX, mY)) {
+                Game.state = STATE.Game;
+                handler.addObject(new Player(r.nextInt(Game.WIDTH2), r.nextInt(Game.HEIGHT2), ID.Player, handler));
+                handler.addObject(new BasicEnemy(r.nextInt(Game.WIDTH2), r.nextInt(Game.HEIGHT2), ID.BasicEnemy, handler));
+            }
+
+            if (mouseOver2(mX, mY)) {
+                Game.state = STATE.About;
+            }
+
+            if (mouseOver3(mX, mY)) {
+                System.exit(1);
+            }
         }
 
-        if (mouseOver3(mX,mY)){
-            System.exit(1);
+        else if (Game.state==STATE.About){
+            if (mouseOver4(mX,mY)){
+                Game.state=STATE.Menu;
+            }
         }
+
     }
 
     public void mouseReleased(MouseEvent e){
@@ -40,8 +53,16 @@ public class Menu extends MouseAdapter {
         return mX > 10 && mX < 10 + 100 && mY > 50 && mY < 50 + 20;
     }
 
+    private boolean mouseOver2(int mX, int mY){
+        return mX > 15 && mX < 15 + 100 && mY > 100 && mY < 100 + 20;
+    }
+
     private boolean mouseOver3(int mX, int mY){
         return mX > 20 && mX < 20 + 100 && mY > 150 && mY < 150 + 20;
+    }
+
+    private boolean mouseOver4(int mX, int mY){
+        return mX > 115 && mX < 115 + 100 && mY > 100 && mY < 100 + 20;
     }
 
     public void tick(){
@@ -49,20 +70,36 @@ public class Menu extends MouseAdapter {
     }
 
     public void render(Graphics g){
-        Font font=new Font("font name", Font.ITALIC,15);
-        g.setFont(font);
+        if (Game.state==STATE.Menu) {
+            Font font = new Font("font name", Font.ITALIC, 15);
+            g.setFont(font);
 
-        g.setColor(Color.white);
-        g.drawRect(10, 50,100,20);
-        g.drawString("START",40,64);
+            g.setColor(Color.white);
+            g.drawRect(10, 50, 100, 20);
+            g.drawString("START", 40, 64);
 
-        g.setColor(Color.lightGray);
-        g.drawRect(15, 100,100,20);
-        g.drawString("ABOUT",45,114);
+            g.setColor(Color.lightGray);
+            g.drawRect(15, 100, 100, 20);
+            g.drawString("ABOUT", 45, 114);
 
-        g.setColor(Color.DARK_GRAY);
-        g.drawRect(20, 150,100,20);
-        g.drawString("OUT",60,164);
+            g.setColor(Color.DARK_GRAY);
+            g.drawRect(20, 150, 100, 20);
+            g.drawString("OUT", 60, 164);
+        }
+
+        else if (Game.state==STATE.About){
+            Font font2=new Font("font name", Font.PLAIN,20);
+            g.setFont(font2);
+            g.setColor(Color.lightGray);
+            g.drawString("2020 春",25,118);
+
+            Font font3=new Font("font name", Font.ITALIC,15);
+            g.setFont(font3);
+            g.setColor(Color.lightGray);
+            g.drawRect(115, 100,100,20);
+            g.drawString("《——",135,114);
+        }
+
     }
 
 }
